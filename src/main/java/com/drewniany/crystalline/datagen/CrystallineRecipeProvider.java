ab3f754bd5db1231197e30f065ae63dce44d254f
Drewniany3000 <mati.zolnowski@gmail.com>
2026-07-09 09:45:33 +0200

    sdasdasd

--- src/main/java/com/drewniany/crystalline/datagen/CrystallineRecipeProvider.java
+++ src/main/java/com/drewniany/crystalline/datagen/CrystallineRecipeProvider.java
@@ -77,6 +77,109 @@ public class CrystallineRecipeProvider extends RecipeProvider {
                 .save(output, "crystalline:x_to_x2");
 
 
+        shaped(RecipeCategory.BUILDING_BLOCKS, CrystallineBlocks.RUBY_BLOCK_X3.get())
+                .pattern("AAA")
+                .pattern("AAA")
+                .pattern("AAA")
+                .define('A', CrystallineItems.FINE_RUBY.get())
+                .unlockedBy(getHasName(CrystallineItems.FINE_RUBY.get()), has(CrystallineItems.FINE_RUBY.get()))
+                .group("Ruby Recipes")
+                .save(output);
+
+        shapeless(RecipeCategory.MISC, CrystallineItems.FINE_RUBY.get(), 9)
+                .requires(CrystallineBlocks.RUBY_BLOCK_X3)
+                .unlockedBy(getHasName(CrystallineBlocks.RUBY_BLOCK_X3.get()), has(CrystallineItems.FINE_RUBY.get()))
+                .group("Ruby Recipes")
+                .save(output);
+
+        shaped(RecipeCategory.BUILDING_BLOCKS, CrystallineBlocks.RUBY_BLOCK_X3.get())
+                .pattern("AAA")
+                .pattern("AAA")
+                .pattern("AAA")
+                .define('A', CrystallineBlocks.RUBY_BLOCK_X2.get())
+                .unlockedBy(getHasName(CrystallineBlocks.RUBY_BLOCK_X2.get()), has(CrystallineBlocks.RUBY_BLOCK_X2.get()))
+                .group("Ruby Recipes")
+                .save(output, "crystalline:x2_to_x3");
+
+
+        shaped(RecipeCategory.BUILDING_BLOCKS, CrystallineBlocks.RUBY_BLOCK_X4.get())
+                .pattern("AAA")
+                .pattern("AAA")
+                .pattern("AAA")
+                .define('A', CrystallineItems.RUBY.get())
+                .unlockedBy(getHasName(CrystallineItems.RUBY.get()), has(CrystallineItems.RUBY.get()))
+                .group("Ruby Recipes")
+                .save(output);
+
+        shapeless(RecipeCategory.MISC, CrystallineItems.RUBY.get(), 9)
+                .requires(CrystallineBlocks.RUBY_BLOCK_X4)
+                .unlockedBy(getHasName(CrystallineBlocks.RUBY_BLOCK_X4.get()), has(CrystallineItems.RUBY.get()))
+                .group("Ruby Recipes")
+                .save(output);
+
+        shaped(RecipeCategory.BUILDING_BLOCKS, CrystallineBlocks.RUBY_BLOCK_X4.get())
+                .pattern("AAA")
+                .pattern("AAA")
+                .pattern("AAA")
+                .define('A', CrystallineBlocks.RUBY_BLOCK_X3.get())
+                .unlockedBy(getHasName(CrystallineBlocks.RUBY_BLOCK_X3.get()), has(CrystallineBlocks.RUBY_BLOCK_X3.get()))
+                .group("Ruby Recipes")
+                .save(output, "crystalline:x3_to_x4");
+
+        shaped(RecipeCategory.BUILDING_BLOCKS, CrystallineBlocks.RUBY_BLOCK_X5.get())
+                .pattern("AAA")
+                .pattern("AAA")
+                .pattern("AAA")
+                .define('A', CrystallineItems.FLAWLESS_RUBY.get())
+                .unlockedBy(getHasName(CrystallineItems.FLAWLESS_RUBY.get()), has(CrystallineItems.FLAWLESS_RUBY.get()))
+                .group("Ruby Recipes")
+                .save(output);
+
+        shapeless(RecipeCategory.MISC, CrystallineItems.FLAWLESS_RUBY.get(), 9)
+                .requires(CrystallineBlocks.RUBY_BLOCK_X5)
+                .unlockedBy(getHasName(CrystallineBlocks.RUBY_BLOCK_X5.get()), has(CrystallineItems.FLAWLESS_RUBY.get()))
+                .group("Ruby Recipes")
+                .save(output);
+
+        shaped(RecipeCategory.BUILDING_BLOCKS, CrystallineBlocks.RUBY_BLOCK_X5.get())
+                .pattern("AAA")
+                .pattern("AAA")
+                .pattern("AAA")
+                .define('A', CrystallineBlocks.RUBY_BLOCK_X4.get())
+                .unlockedBy(getHasName(CrystallineBlocks.RUBY_BLOCK_X4.get()), has(CrystallineBlocks.RUBY_BLOCK_X4.get()))
+                .group("Ruby Recipes")
+                .save(output, "crystalline:x4_to_x5");
+
+        shaped(RecipeCategory.BUILDING_BLOCKS, CrystallineBlocks.RUBY_BLOCK_X6.get())
+                .pattern("AAA")
+                .pattern("AAA")
+                .pattern("AAA")
+                .define('A', CrystallineItems.PERFECT_RUBY.get())
+                .unlockedBy(getHasName(CrystallineItems.PERFECT_RUBY.get()), has(CrystallineItems.PERFECT_RUBY.get()))
+                .group("Ruby Recipes")
+                .save(output);
+
+        shapeless(RecipeCategory.MISC, CrystallineItems.PERFECT_RUBY.get(), 9)
+                .requires(CrystallineBlocks.RUBY_BLOCK_X6)
+                .unlockedBy(getHasName(CrystallineBlocks.RUBY_BLOCK_X6.get()), has(CrystallineItems.PERFECT_RUBY.get()))
+                .group("Ruby Recipes")
+                .save(output);
+
+        shaped(RecipeCategory.BUILDING_BLOCKS, CrystallineBlocks.RUBY_BLOCK_X6.get())
+                .pattern("AAA")
+                .pattern("AAA")
+                .pattern("AAA")
+                .define('A', CrystallineBlocks.RUBY_BLOCK_X5.get())
+                .unlockedBy(getHasName(CrystallineBlocks.RUBY_BLOCK_X5.get()), has(CrystallineBlocks.RUBY_BLOCK_X5.get()))
+                .group("Ruby Recipes")
+                .save(output, "crystalline:x5_to_x6");
+
+
+
+
+
+
+
 
     }
 }
